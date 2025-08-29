c2bd30dfc93b21fae21ba27c3b7d5f56589dcc04
Valeria-git <62391065+Valeria-git@users.noreply.github.com>
2025-08-29 13:05:26 +0200

    Update conftest.py

--- part1/conftest.py
+++ part1/conftest.py
@@ -10,21 +10,20 @@ def page(request):
         browser = p.chromium.launch(headless=False)
         context = browser.new_context()
 
-        # Включаем трассировку (для всех шагов)
+       
         context.tracing.start(screenshots=True, snapshots=True, sources=True)
         page = context.new_page()
 
         yield page
 
-        # После теста проверяем его результат
+        
         if request.node.rep_call.failed:
-            # Сохраняем трассировку только для упавшего теста
+            
             test_name = request.node.name
             trace_path = f"traces/{test_name}-trace.zip"
             context.tracing.stop(path=trace_path)
             print(f"\nTrace saved: {trace_path}")
         else:
-            # Если тест успешен, просто останавливаем трассировку без сохранения
             context.tracing.stop()
         
         browser.close()
